acf6f9db85d9902a2b7c58a9438179040a26f6f8
brnnnrsmssn <brnnnrsmssn@gmail.com>
2017-11-03 17:33:39 -0500

    began setup for actions experience values

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 /*
 	GLOSSAARY
-
 	- experientia: the accumulation of a characters experiences. used to distinguish between experiences{this} (events) from experience points{!this} (stats).
-
 	- experience points [experience, exp, xp]: granted to players for doing actions. they are the underlying metric for a characters stats.
-
 	- statistics [stats, abilities]: the measure of a character. divided into six categories: strength, dexterity, constitution, intelligence, wisdom, charisma. governs the chance for success when performing actions.
-
 	- strength [str]:
-
 	- dexterity [dex]:
-
 	- constitution [con]:
-
 	- intelligence [int]:
-
 	- wisdom [wis]:
-
 	- charisma [cha]:
-
 	- modifier [mod]:
 */
 
@@ -42,8 +32,6 @@ function playerCharacter() { //character constructor
 	this.wis = new Array (score:undefined, mod:undefined, exp:undefined);
 	//charisma variables
 	this.cha = new Array (score:undefined, mod:undefined, exp:undefined);
-
-	
 }
 
 function findCharacter(pc) { //iterates through all the character sheets and returns the correstponding sheet to a character object
@@ -73,11 +61,12 @@ function pullCharacter(pc) { //pulls character information from the character sh
 function pushCharacter(pc) { //pushes updated character stats to the correct character sheet
 
 	/*
-	get pc name 
+	find character
+	push pc object stats to sheet
 	*/
 }
 
-function didAThing(pc,stat,exp) { //implements the application of experientia to a players stats
+function didAThing(/*pc,stat,exp*/) { //implements the application of experientia to a players stats
 	
 	/*
 	pc.
@@ -89,3 +78,11 @@ function didAThing(pc,stat,exp) { //implements the application of experientia to
 /////////////////////////////////////////////////////////////////////////////////////////
 /*   CONSTANTS   */
 /////////////////////////////////////////////////////////////////////////////////////////
+
+var ACTIONS = new Array ();
+var STR_ACTIONS = ACTIONS[0] = new Array ();
+var DEX_ACTIONS = ACTIONS[1] = new Array ();
+var CON_ACTIONS = ACTIONS[2] = new Array ();
+var INT_ACTIONS = ACTIONS[3] = new Array ();
+var WIS_ACTIONS = ACTIONS[4] = new Array ();
+var CHA_ACTIONS = ACTIONS[5] = new Array ();
